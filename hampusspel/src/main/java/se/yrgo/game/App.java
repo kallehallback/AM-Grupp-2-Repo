bf64cdfc705ef3147399b671846db8d0f3f384e9
David Skansholm <valkeshar@gmail.com>
2026-03-23 00:56:12 +0100

    Uppdaterade kod
    Lade in begränsningar i y-axel

---
+++ hampusspel/src/main/java/se/yrgo/game/App.java
@@ -0,0 +1,23 @@
+package se.yrgo.game;
+
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+/**
+ * This is a very small "game" just to show the absolute basics of
+ * how to draw on a surface in a frame using Swing/AWT.
+ * 
+ */
+public class App {
+    public static void main(String[] args) {
+        JFrame main = new JFrame("Jumpy Birb");
+
+        GameSurface gs = new GameSurface(400);
+
+        main.setSize(1536, 864);
+        main.add(gs);
+        main.addKeyListener(gs);
+        main.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        main.setVisible(true);
+    }
+}
